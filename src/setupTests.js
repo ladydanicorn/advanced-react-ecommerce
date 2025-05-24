a97ca5150329b43bfbe53d1956b7f1dcdbc42183
ladydanicorn <dbronsonart@gmail.com>
2025-05-24 14:56:34 -0400

    fix: use inline JSX in mocks to avoid React out-of-scope error

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,18 +1,14 @@
-import React from 'react';
-
-// Mock react-router-dom
 jest.mock('react-router-dom', () => ({
-  BrowserRouter: ({ children }) => React.createElement('div', { 'data-testid': 'browser-router' }, children),
-  Routes: ({ children }) => React.createElement('div', { 'data-testid': 'routes' }, children),
-  Route: ({ children }) => React.createElement('div', { 'data-testid': 'route' }, children),
-  Navigate: () => React.createElement('div', { 'data-testid': 'navigate' }, 'Navigate'),
-  Link: ({ children, to }) => React.createElement('a', { href: to, 'data-testid': 'link' }, children),
+  BrowserRouter: ({ children }) => <div data-testid="browser-router">{children}</div>,
+  Routes: ({ children }) => <div data-testid="routes">{children}</div>,
+  Route: ({ children }) => <div data-testid="route">{children}</div>,
+  Navigate: () => <div data-testid="navigate">Navigate</div>,
+  Link: ({ children, to }) => <a href={to} data-testid="link">{children}</a>,
   useNavigate: () => jest.fn(),
   useParams: () => ({}),
   useLocation: () => ({ pathname: '/' }),
 }));
 
-// Mock react-i18next
 jest.mock('react-i18next', () => ({
   useTranslation: () => ({
     t: (key) => key,
@@ -23,12 +19,11 @@ jest.mock('react-i18next', () => ({
   }),
 }));
 
-// Mock components
-jest.mock('./components/auth/Login', () => () => React.createElement('div', { 'data-testid': 'login-component' }, 'Login Component'));
-jest.mock('./components/auth/Register', () => () => React.createElement('div', { 'data-testid': 'register-component' }, 'Register Component'));
-jest.mock('./components/user/Profile', () => () => React.createElement('div', { 'data-testid': 'profile-component' }, 'Profile Component'));
-jest.mock('./components/products/ProductList', () => () => React.createElement('div', { 'data-testid': 'product-list-component' }, 'ProductList Component'));
-jest.mock('./components/products/ProductDetail', () => () => React.createElement('div', { 'data-testid': 'product-detail-component' }, 'ProductDetail Component'));
-jest.mock('./components/cart/Cart', () => () => React.createElement('div', { 'data-testid': 'cart-component' }, 'Cart Component'));
-jest.mock('./components/common/Navbar', () => () => React.createElement('div', { 'data-testid': 'navbar-component' }, 'Navbar Component'));
-jest.mock('./components/orders/OrderHistory', () => () => React.createElement('div', { 'data-testid': 'order-history-component' }, 'OrderHistory Component'));
+jest.mock('./components/auth/Login', () => () => <div data-testid="login-component">Login Component</div>);
+jest.mock('./components/auth/Register', () => () => <div data-testid="register-component">Register Component</div>);
+jest.mock('./components/user/Profile', () => () => <div data-testid="profile-component">Profile Component</div>);
+jest.mock('./components/products/ProductList', () => () => <div data-testid="product-list-component">ProductList Component</div>);
+jest.mock('./components/products/ProductDetail', () => () => <div data-testid="product-detail-component">ProductDetail Component</div>);
+jest.mock('./components/cart/Cart', () => () => <div data-testid="cart-component">Cart Component</div>);
+jest.mock('./components/common/Navbar', () => () => <div data-testid="navbar-component">Navbar Component</div>);
+jest.mock('./components/orders/OrderHistory', () => () => <div data-testid="order-history-component">OrderHistory Component</div>);
